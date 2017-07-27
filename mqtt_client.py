bd6f053bb81a69efe7c5386de005412bbb4b286f
ak <apugba16@gmail.com>
2017-07-27 15:48:12 +0000

    initial commit

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,30 +1,29 @@
-
 import paho.mqtt.client as mqtt
+from RPLCD import CharLCD
+import time
+lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
 
 
-
-LCD_RS = 26
-LCD_E  = 19
-LCD_D4 = 13 
-LCD_D5 = 6
-LCD_D6 = 5
-LCD_D7 = 11
-LED_ON = 15
-         O
+def becca(message):
+	lcd.write_string(message.payload)
+	time.sleep(10)
+	lcd.clear()
 def on_message(client, userdata, message):
 	print message.payload
-	message_type(message)
-def message_type(client, userdate, message):
-	print message
+	becca(message)
+	
  
 def on_connect(client, userdata, flags, code):
         print "connected:" + str(code)
-        client.subscribe("test/apugba123")
+        client.subscribe("test/motion")
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("broker.hivemq.com", 1883, 60)
+
 client.loop_forever()
+
+
 
