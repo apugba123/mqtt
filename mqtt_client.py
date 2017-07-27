0bdc4fc25ef37285cd097a7ffca26df736d9a004
ak <apugba16@gmail.com>
2017-07-27 15:57:04 +0000

    initial commit

--- mqtt_client.py
+++ mqtt_client.py
@@ -6,7 +6,7 @@ lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
 
 def becca(message):
 	lcd.write_string(message.payload)
-	time.sleep(10)
+	time.sleep(5)
 	lcd.clear()
 def on_message(client, userdata, message):
 	print message.payload
